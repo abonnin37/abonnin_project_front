338b97520d78ef59bbb68ed7ac07ae856c327c50
abonnin37 <bonnin.a.k@gmail.com>
2021-05-22 15:21:40 -0300

    Bloc - Blog redirection
    Update <a> link to <Link> component from react-router-dom

--- src/components/MainPage/blog-redirection/blog-redirection.js
+++ src/components/MainPage/blog-redirection/blog-redirection.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 import style from "./blog-redirection.module.scss";
 
 const BlogRedirection = () => {
     return (
         <div className={style.blogRedirection}>
-            <h2>Click <a href="">here</a> to go to the blog</h2>
+            <h2>Click <Link to="/blog">here</Link> to go to the blog</h2>
         </div>
     );
 }

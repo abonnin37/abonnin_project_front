54e9cde6f81e4d33cde0162b330aade91abb25f0
abonnin37 <bonnin.a.k@gmail.com>
2021-05-22 14:58:45 -0300

    Bloc - Home Hero
    - desktop integration
    - responsive

--- src/components/MainPage/home-hero/home-hero.js
+++ src/components/MainPage/home-hero/home-hero.js
@@ -9,23 +9,22 @@ const HomeHero = () => {
         <div className={style.homeHero}>
             <h1 className={style.title}>Alexandre Bonnin</h1>
             <div className={style.contentHomeHero}>
-                <div className={style.colLeft}>
-                    <div className={style.cvLink}>
-                        <RightArrow />
-                        <a href="">Link to my CV</a>
-                    </div>
-                    <div className={style.banner}>
-                        <img src={Banner} alt=""/>
-                    </div>
-                    <div className={style.descriptionGroup}>
-                        <h2>
-                            Je suis un ingénieur en informatique avec un fort penchant pour le développement web et l'entrepreneuriat.
-                        </h2>
-                        <p>
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam aspernatur, commodi culpa eius est id impedit incidunt ipsum maxime nostrum, obcaecati perspiciatis quo, quod repellat sint tempore temporibus vitae voluptates?
-                        </p>
-                    </div>
+                <div className={style.cvLink}>
+                    <RightArrow />
+                    <a href="">Link to my CV</a>
                 </div>
+                <div className={style.banner}>
+                    <img src={Banner} alt=""/>
+                </div>
+                <div className={style.descriptionGroup}>
+                    <h2>
+                        Je suis un ingénieur en informatique avec un fort penchant pour le développement web et l'entrepreneuriat.
+                    </h2>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam aspernatur, commodi culpa eius est id impedit incidunt ipsum maxime nostrum, obcaecati perspiciatis quo, quod repellat sint tempore temporibus vitae voluptates?
+                    </p>
+                </div>
+
                 <div className={style.colRight}>
                     <div className={style.slash}>
                     </div>

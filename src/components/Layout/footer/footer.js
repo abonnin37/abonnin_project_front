44448354843410b171fb709f045d8e73646426b9
abonnin37 <bonnin.a.k@gmail.com>
2021-05-22 16:13:27 -0300

    Bloc - Footer

--- src/components/Layout/footer/footer.js
+++ src/components/Layout/footer/footer.js
@@ -2,20 +2,33 @@ import React from 'react';
 
 import style from './footer.module.scss';
 import Logo from "/src/assets/images/logo.svg";
+import {NavLink} from "react-router-dom";
 
 const Footer = () => {
     return (
         <footer className={style.footer}>
             <div className={style.logoGroup}>
                 <Logo />
-                <h4>Alexandre Bonnin</h4>
+                <h1>Alexandre Bonnin</h1>
             </div>
-            <div className={style.menu}>
+            <div className={style.linksGroup}>
 
             </div>
+            <div className={style.menu}>
+                <nav>
+                    <ul>
+                        <li>
+                            <NavLink activeClassName={style.active} to="/acceuil">Acceuil</NavLink>
+                        </li>
+                        <li>
+                            <NavLink activeClassName={style.active} to="/blog">Blog</NavLink>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
             <div className={style.outerLinks}>
-                <a href="">linkedin</a>
-                <a href="">malt</a>
+                <a target="_blank" href="https://www.linkedin.com/in/alexandre-bonnin-224574153/">linkedin</a>
+                <a target="_blank" href="https://www.malt.fr/profile/alexandrebonnin">malt</a>
             </div>
         </footer>
     );

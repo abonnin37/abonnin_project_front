f009dc99cee5c97c8045bab8d28a413170c0ad8c
abonnin37 <bonnin.a.k@gmail.com>
2021-05-30 17:53:52 -0300

    Add & configure Axios

--- src/components/MainPage/projects/projects.js
+++ src/components/MainPage/projects/projects.js
@@ -1,8 +1,23 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import axios from "../../../axios";
 
 import style from "./projects.module.scss";
 
 const Projects = () => {
+    const [projects, setProjects] = useState([]);
+
+    useEffect(() => {
+        axios.get('/users/2/projects')
+            .then((response) => {
+                setProjects(response.data['hydra:member']);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+
+    }, []);
+
+    console.log(projects);
     return (
         <div className={style.projects}>
             <h1 className={style.title}>My projects</h1>
